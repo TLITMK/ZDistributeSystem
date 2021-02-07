1d69f2e90cc397cd3d15b3016554852d04b992d9
tlitmk <18208801176@163.com>
2021-02-07 18:30:44 +0800

    用户表新增openid字段

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('account')->unique();
+            $table->string('account')->unique()->comment('微信小程序用户名 未绑定手机默认为openid 绑定手机后设置为手机号');
             $table->string('password');
             $table->string('nickname', 32)->comment('昵称');
             $table->unsignedTinyInteger('gender')->default(3)->comment('性别 1 男 2 女 3 未知');
@@ -23,6 +23,7 @@ class CreateUsersTable extends Migration
             $table->string('avatar', 128)->nullable();
             $table->string('email')->unique();
             $table->string('signature')->nullable();
+            $table->string('open_id')->nullable();//用于小程序用户记录open_id，访问时触发新增用户，？？更新？？用户服务端查找redis中的session_key和小程序登陆的自定义唯一密钥
             $table->rememberToken();
             $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
